968b43b236c1b4fe243a86dd5e6234d94fe34cb6
MoRayyan107 <166967645+MoRayyan107@users.noreply.github.com>
2025-04-27 04:02:58 +0100

    small change on Task.java

--- src/main/java/com/manager/TaskManagerAPI/model/Task.java
+++ src/main/java/com/manager/TaskManagerAPI/model/Task.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.NotBlank;
 
 @Entity // creates a table into a local database (H2)
@@ -14,7 +16,10 @@ public class Task {
     private Long id;
 
     @NotBlank(message = "Title cannot be Empty")
+    @Size(max = 100, message = "title must be less than 100 characters!")
     private String title;
+
+    @NotNull(message = "Description cannot be empty!")
     private String description;
     private boolean completed;
 
